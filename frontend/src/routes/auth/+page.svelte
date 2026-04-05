e622650fe65efeb6c6625e605014b120ad2f0e5e
XrystikLK <23spiisiinii@gmail.com>
2026-04-05 12:05:26 +0500

    feat(auth): very simple auth

---
+++ frontend/src/routes/auth/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import LoginForm from '$lib/components/auth/LoginForm.svelte';
+</script>
+
+<div class="flex flex-col h-screen w-full items-center justify-center px-4">
+	<!-- <h1>Welcome to SvelteKit</h1>
+	<p>
+		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+	</p> -->
+	<LoginForm />
+</div>
---
+++ frontend/src/routes/dashboard/+layout.server.ts
@@ -0,0 +1,9 @@
+import { redirect } from '@sveltejs/kit';
+import type { LayoutServerLoad } from './$types';
+
+export const load: LayoutServerLoad = ({cookies}) => {
+    if (cookies.get('isAuthenticated') !== 'true') {
+        throw redirect(302, '/auth');
+    }
+
+};
