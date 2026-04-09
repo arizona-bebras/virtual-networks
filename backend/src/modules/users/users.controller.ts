a1c378520d2bf1b01cf3d43897e09caac423a62d
shyinakira <maxi.solts@gmail.com>
2026-04-09 15:34:17 +0500

    feat: user CRUD

--- backend/src/modules/users/users.controller.ts
+++ backend/src/modules/users/users.controller.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import {
   Body,
   Controller,
@@ -9,11 +10,17 @@ import {
 } from "@nestjs/common";
 import type { User } from "./interfaces/user.interface";
 import type { UsersService } from "./users.service";
+=======
+import { Controller, Get, Param, Post, Body, Delete, Put  } from '@nestjs/common';
+import { UsersService } from './users.service';
+import type { User } from './interfaces/user.interface';
+>>>>>>> 44c3127 (feat: user CRUD)
 
 @Controller("users")
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+<<<<<<< HEAD
   @Post()
   create(@Body() user: User) {
     return this.usersService.create(user);
@@ -33,4 +40,25 @@ export class UsersController {
   delete(@Param("id") id: string) {
     return this.usersService.delete(id);
   }
+=======
+    @Post()
+    create(@Body() user: User) {
+        return this.usersService.create(user);
+    }
+
+    @Get(':id')
+    read(@Param('id') id: string) {
+        return this.usersService.read(id);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() user: User) {
+        return this.usersService.update(id, user);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string) {
+        return this.usersService.delete(id);
+    }
+>>>>>>> 44c3127 (feat: user CRUD)
 }
