44c312705e3868645003d7a5b140afc60a95c3a3
shyinakira <maxi.solts@gmail.com>
2026-04-09 15:34:17 +0500

    feat: user CRUD

--- backend/src/modules/users/users.controller.ts
+++ backend/src/modules/users/users.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get, Param, Post,  } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Delete, Put  } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { User } from './interfaces/user.interface';
+import type { User } from './interfaces/user.interface';
 
 @Controller('users')
 export class UsersController {
@@ -9,7 +9,20 @@ export class UsersController {
     @Post()
     create(@Body() user: User) {
         return this.usersService.create(user);
-    } 
-    
-}
+    }
 
+    @Get(':id')
+    read(@Param('id') id: string) {
+        return this.usersService.read(id);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() user: User) {
+        return this.usersService.update(id, user);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string) {
+        return this.usersService.delete(id);
+    }
+}
