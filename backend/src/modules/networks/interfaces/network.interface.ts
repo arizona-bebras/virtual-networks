2dd52cce1d620cb7e26b7b7c0f058f2977542940
shyinakira <maxi.solts@gmail.com>
2026-04-09 16:43:24 +0500

    feat: network CRUD

---
+++ backend/src/modules/networks/interfaces/network.interface.ts
@@ -0,0 +1,21 @@
+export interface Network {
+  name: string;
+  description: string;
+  ip: string;
+  subnet: number;
+  config: string;
+  admin_id: string;
+}
+
+export interface Device {
+    name: string;
+    ip: string;
+    config: string;
+    network_id: string;
+}
+
+export interface Tag {
+    name: string;
+    rules: string;
+    network_id: string;
+}
--- backend/src/modules/networks/networks.controller.ts
+++ backend/src/modules/networks/networks.controller.ts
@@ -1,4 +1,84 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Put, Delete } from '@nestjs/common';
+import { NetworksService } from './networks.service';
+import { DevicesService } from './devices/devices.service';
+import { TagsService } from './tags/tags.service';
+import type { Network, Device, Tag } from './interfaces/network.interface';
+
 
 @Controller('networks')
-export class NetworksController {}
+export class NetworksController {
+    constructor(
+        private readonly networksService: NetworksService,
+        private readonly devicesService: DevicesService,
+        private readonly tagsService: TagsService
+    ) {}
+
+    /**NETWORK */
+
+    @Post()
+    async create(@Body() network: Network) {
+        await this.networksService.create(network);
+    }
+
+    @Get(':network_id')
+    async get(@Param('network_id') id: string) {
+        return await this.networksService.read(id);
+    }
+
+    @Put(':network_id')
+    async update(@Param('network_id') id: string, @Body() network: Network) {
+        await this.networksService.update(id, network);
+    }
+
+    @Delete(':network_id')
+    async delete(@Param('network_id') id: string) {
+        await this.networksService.delete(id);
+    }
+
+
+    /**DEVICES */
+
+    @Post(':network_id/devices')
+    async createDevice(@Param('network_id') network_id: string, @Body() device: Device) {
+        await this.devicesService.create(device, network_id);
+    }
+
+    @Get(':network_id/devices/:device_id')
+    async getDevice(@Param('device_id') id: string) {
+        return await this.devicesService.read(id);
+    }
+
+    @Put(':network_id/devices/:device_id')
+    async updateDevice(@Param('device_id') id: string, @Body() device: Device) {
+        await this.devicesService.update(id, device);
+    }
+
+    @Delete(':network_id/devices/:device_id')
+    async deleteDevice(@Param('device_id') id: string) {
+        await this.devicesService.delete(id);
+    }
+
+    /**TAGS */
+
+    @Post(':network_id/tags')
+    async createTag(@Param('network_id') network_id: string, @Body() tag: Tag) {
+        await this.tagsService.create(tag, network_id);
+    }
+
+    @Get(':network_id/tags/:tag_id')
+    async getTag(@Param('tag_id') id: string) {
+        return await this.tagsService.read(id);
+    }
+
+    @Put(':network_id/tags/:tag_id')
+    async updateTag(@Param('tag_id') id: string, @Body() tag: Tag) {
+        await this.tagsService.update(id, tag);
+    }
+
+    @Delete(':network_id/tags/:tag_id')
+    async deleteTag(@Param('tag_id') id: string) {
+        await this.tagsService.delete(id);
+    }
+
+
+}
