a3de706e8915d6fad1d85b442d8cb301f24def96
shyinakira <maxi.solts@gmail.com>
2026-04-15 17:01:55 +0500

    fix:linter nest-js conf

--- backend/src/modules/rules/rules.service.spec.ts
+++ backend/src/modules/rules/rules.service.spec.ts
@@ -1,4 +1,4 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test, type TestingModule } from '@nestjs/testing';
 import { RulesService } from './rules.service';
 
 describe('RulesService', () => {
