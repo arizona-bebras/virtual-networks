c157da4b91da0668cdaf702aee7b6383340905e8
XrystikLK <23spiisiinii@gmail.com>
2026-04-20 21:35:42 +0500

    feat(frontend): auth using better auth

--- frontend/src/routes/app/+page.svelte
+++ frontend/src/routes/app/+page.svelte
@@ -1,12 +1,5 @@
 <script lang="ts">
-import { onMount } from "svelte";
-import { goto } from "$app/navigation";
-
-onMount(() => {
-  goto("/app/dashboard");
-});
+import Page from "$pages/root/ui/page.svelte";
 </script>
 
-<div class="flex h-screen items-center justify-center">
-  <p class="text-muted-foreground animate-pulse">Redirecting...</p>
-</div>
+<Page />
--- frontend/src/routes/auth/+layout.ts
+++ frontend/src/routes/auth/+layout.ts
@@ -1,7 +1,16 @@
-import { redirect } from "@sveltejs/kit";
+// import { redirect } from "@sveltejs/kit";
 
-export const load = () => {
-  if (localStorage.getItem("token")) {
+// export const load = () => {
+//   if (localStorage.getItem("token")) {
+//     throw redirect(302, "/app/dashboard");
+//   }
+// };
+import { redirect } from "@sveltejs/kit";
+import { authClient } from "$shared/api/auth-client";
+
+export const load = async () => {
+  const { data } = await authClient.getSession();
+  if (data) {
     throw redirect(302, "/app/dashboard");
   }
 };
