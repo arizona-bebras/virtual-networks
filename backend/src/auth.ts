c1ae5e32cdec9c7330188d663c26bc6549885cbc
XrystikLK <23spiisiinii@gmail.com>
2026-04-20 20:11:34 +0500

    feat(backend): add CORS for nest, betterAuth. BetterAuth localization

--- backend/src/auth.ts
+++ backend/src/auth.ts
@@ -3,6 +3,7 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { jwt } from "better-auth/plugins";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { postgresUrl } from "./db/connection";
+import { localization } from "better-auth-localization";
 import * as schema from "./db/schema";
 import { relations } from "./db/schema";
 
@@ -18,7 +19,14 @@ const authOptions: BetterAuthOptions = {
   emailAndPassword: {
     enabled: true,
   },
-  plugins: [jwt()],
+  trustedOrigins: ["http://localhost:5173"],
+  plugins: [
+    jwt(),
+    localization({
+      defaultLocale: "ru-RU", 
+      fallbackLocale: "default",
+    }),
+  ],
 };
 
 export const auth = betterAuth(authOptions);
