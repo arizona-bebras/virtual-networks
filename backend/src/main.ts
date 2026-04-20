d40bbe1e43ad2cfc1fd0f26322e78dc11b9529af
XrystikLK <23spiisiinii@gmail.com>
2026-04-20 20:11:34 +0500

    feat(backend): add CORS for nest, betterAuth. BetterAuth localization

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,13 +8,18 @@ async function bootstrap() {
     // Better Auth will re-add the default body parsers for non-auth routes.
     bodyParser: false,
   });
+  app.enableCors({
+    origin: ["http://localhost:5173"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    credentials: true,
+  });
   const config = new DocumentBuilder()
     .setTitle("Virtual Networks API")
     .setDescription("API for managing virtual networks")
     .setVersion("1.0")
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("api", app, document);
+  SwaggerModule.setup("api/docs", app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
