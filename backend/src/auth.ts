ed6875db709eb70584851decc7fc24b4d613365d
Lowder <me@lowderplay.dev>
2026-04-20 22:19:46 +0500

    fix: sort imports

--- backend/src/auth.ts
+++ backend/src/auth.ts
@@ -1,9 +1,9 @@
 import { type BetterAuthOptions, betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { jwt } from "better-auth/plugins";
+import { localization } from "better-auth-localization";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { postgresUrl } from "./db/connection";
-import { localization } from "better-auth-localization";
 import * as schema from "./db/schema";
 import { relations } from "./db/schema";
 
