e39f9d672a4a0d09624e81c99ebf90e1f8bcdaf8
LowderPlay <me@lowderplay.dev>
2026-04-20 18:39:33 +0500

    refactor: move db config to a single file (#44)

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -1,17 +1,11 @@
-import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
-
-const host = process.env.POSTGRES_HOST ?? "localhost";
-const port = Number(process.env.POSTGRES_PORT) ?? 5432;
-const user = process.env.POSTGRES_USER ?? "postgres";
-const password = process.env.POSTGRES_PASSWORD ?? "";
-const database = process.env.POSTGRES_DB ?? "postgres";
+import { postgresUrl } from "./src/db/connection";
 
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: `postgresql://${user}:${password}@${host}:${port}/${database}`,
+    url: postgresUrl,
   },
 });
--- backend/src/db/database.module.ts
+++ backend/src/db/database.module.ts
@@ -2,15 +2,8 @@ import { Global, Module } from "@nestjs/common";
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
+import { postgresUrl } from "./connection";
 import * as schema from "./schema";
-
-const HOST = process.env.POSTGRES_HOST ?? "localhost";
-const PORT = Number(process.env.POSTGRES_PORT ?? 5432);
-const USER = process.env.POSTGRES_USER ?? "postgres";
-const PASSWORD = process.env.POSTGRES_PASSWORD ?? "";
-const DATABASE = process.env.POSTGRES_DB ?? "postgres";
-
-const URL = `postgresql://${USER}:${PASSWORD}@${HOST}:${PORT}/${DATABASE}`;
 
 export const DRIZZLE = "DRIZZLE";
 
@@ -21,7 +14,7 @@ export const DRIZZLE = "DRIZZLE";
       provide: DRIZZLE,
       useFactory: () => {
         const pool = new Pool({
-          connectionString: URL,
+          connectionString: postgresUrl,
         });
         return drizzle({ client: pool, schema: schema });
       },
