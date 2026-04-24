5b235f60c7ff182ea7841f8ad37c31598f2e6cab
XrystikLK <23spiisiinii@gmail.com>
2026-04-24 18:42:56 +0500

    feat(fronted): change root +page redirection URL

--- frontend/src/pages/root/ui/page.svelte
+++ frontend/src/pages/root/ui/page.svelte
@@ -3,8 +3,7 @@ import { onMount } from "svelte";
 import { goto } from "$app/navigation";
 
 onMount(() => {
-  const token = localStorage.getItem("token");
-  goto(token ? "/app/" : "/auth/login");
+  goto("/app/");
 });
 </script>
 
