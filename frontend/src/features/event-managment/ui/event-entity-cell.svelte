989807c745d41c7a05527098aa1892af1c7e9feb
XrystikLK <23spiisiinii@gmail.com>
2026-06-01 12:52:07 +0500

    feat(events): add redirect link to network entities

--- frontend/src/features/event-managment/ui/event-entity-cell.svelte
+++ frontend/src/features/event-managment/ui/event-entity-cell.svelte
@@ -1,18 +1,11 @@
 <script lang="ts">
 import { MonitorSmartphone, Settings, Shield, Tag } from "@lucide/svelte";
-import type { Device } from "common/schemas/device/index";
 import type { EventEntity } from "common/schemas/event/index";
-import type { Rule } from "common/schemas/rule/index";
 import { getNetworkId } from "$shared/lib/network-id-context";
 
 let { entity }: { entity: EventEntity } = $props();
 
 let currentNetwork = $derived(getNetworkId().id);
-
-let isDevice = $derived(entity.type === "device");
-let isRule = $derived(entity.type === "rule");
-let isTag = $derived(entity.type === "tag");
-let isNetwork = $derived(entity.type === "network");
 
 let redirectTo = $derived.by(() => {
   let basePath = `/app/network/${currentNetwork}`;
@@ -20,6 +13,7 @@ let redirectTo = $derived.by(() => {
   else if (entity.type === "rule")
     basePath += `/rules?description=${entity.info.description}`;
   else if (entity.type === "tag") basePath += `/tags?name=${entity.info.name}`;
+  else if (entity.type === "network") basePath += "/config";
   return basePath;
 });
 </script>
